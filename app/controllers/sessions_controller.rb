9ebb5adc43a97545742f785018b4a9cefe80f18a
Catherine Khuu & Scott Dixon & Lindsay Kelly <pair+catkhuu.dixonscottr.lindsaymkelly@devbootcamp.com>
2016-11-17 16:33:17 -0500

    Add login logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      flash[:notice] = "Thank you for logging in!"
+      session[:user_id]= @user.id
+      redirect_to '/'
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    session.clear
+    flash[:notice] = 'You are logged out and will be missed :('
+    redirect_to '/'
   end
 
 end
